867d71b1697d867d00fc99c7aa87149928f0d8a7
Paulo Cesar <email@pocesar.e4ward.com>
2014-01-28 07:46:50 -0200

    preliminary stuff

--- README.md
+++ README.md
@@ -1,4 +1,58 @@
-node-stratum-solo
+Stratum Solo Mining
 =================
 
-Stratum server for solo mining for any daemon in Node.js
+Stratum server for solo mining for any daemon SHA256 and Litecoin in Node.js
+
+You can create a multi coin solo mining central, just assign multiple daemons then change,
+and watch the values of most profitable coin using Cryptsy API.
+
+You should use this to have one server that holds your daemons, and make a lot of miners to connect to it,
+it can handle a hell alot of traffic and still be performant.
+
+Install
+=================
+
+Directly from github (easier):
+
+```bash
+git clone git@github.com:pocesar/node-stratum-solo.git
+cd node-stratum-solo
+node .
+``
+
+or through NPM:
+
+```bash
+npm install stratum-solo
+cd node_modules/stratum-solo
+node .
+```
+
+Usage
+=================
+
+It comes with litecoin and bitcoin enabled that will work on testnet by default, because it's mainly a PoC.
+
+You should edit `index.js` with your settings. It's almost sure it won't work out of the box,
+since the path of the daemons are hardcoded, you should change it to match your system. Also the daemon
+must accept `getblocktemplate` RPC command, and you must provide the proper credentials
+in the `index.js` file.
+
+Support
+=================
+
+`BTC: 1PskTzQjmzqB2boz67AXsv4C5YNWN4xmhu`
+
+
+`LTC: LW2kXiquqDu3GfpfBX2xNTSgjVmBPu6g3z`
+
+
+`PPC: PNKZEkDf9eBSNebu2CcxHaGuma6wHuZEBh`
+
+
+`XPM: ARKZ7uVE1YS19HC8jSq5xZGkr6YAzugWBv`
+
+
+```bash
+npm star stratum-solo
+```
---
+++ index.js
@@ -0,0 +1,126 @@
+'use strict';
+
+var
+  Stratum = require('stratum'),
+  servers = {
+    scrypt: {
+      daemons: {
+        litecoin: {
+          name    : 'litecoin',
+          path    : 'G:\\Dropbox\\Coins\\LTC\\daemon\\litecoind.exe',
+          datadir : 'G:\\Dropbox\\Coins\\LTC\\data',
+          user    : 'user',
+          password: 'password',
+          port    : 9332,
+          host    : '127.0.0.1',
+          args    : [
+            'testnet'
+          ]
+        }
+      },
+      server : new Stratum.Server({
+        settings: {
+          port: 3334
+        }
+      })
+    },
+    sha256: {
+      daemons: {
+        bitcoin: {
+          name    : 'bitcoin',
+          path    : 'G:\\Dropbox\\Coins\\BTC\\daemon\\bitcoind.exe',
+          datadir : 'G:\\Dropbox\\Coins\\BTC\\data',
+          user    : 'user',
+          password: 'password',
+          port    : 8332,
+          host    : '127.0.0.1',
+          args    : [
+            'testnet'
+          ]
+        }
+      },
+      server : new Stratum.Server({
+        settings: {
+          port: 3333
+        }
+      })
+    }
+  },
+  daemon;
+
+var setup = function(type, coin){
+  coin.server.on('mining', function (req, deferred, socket){
+    console.log(req);
+
+    switch (req.method) {
+      case 'subscribe':
+        /* miner is asking for subscription */
+        break;
+      case 'authorize':
+        /* miner is asking for authorization */
+        break;
+      case 'submit':
+        /* miner is submitting a share */
+        break;
+      default:
+        deferred.reject(Stratum.Server.errors.METHOD_NOT_FOUND);
+    }
+  });
+
+  coin.server.on('rpc', function (name, args, connection, deferred){
+    switch (name) {
+      case 'mining.block':
+        var daemon = args[1];
+        if (typeof coin.daemons[daemon] !== 'undefined') {
+          coin.server.broadcast('notify');
+
+        }
+        deferred.resolve(['ok']);
+        break;
+    }
+  });
+
+  coin.server.on('mining.error', function(message){
+    console.log(type + ' error: ', message);
+  });
+
+  coin.server.listen().then(function (port){
+    // Start all our daemons
+    for (var daemon in coin.daemons) {
+      if (coin.daemons[daemon].start()) {
+        (function(d){
+          console.log('Starting ' + d.name);
+          
+          d.on('close', function(){
+            console.log(d.name + ' process closed');
+          });
+
+          setTimeout(function(){
+            d.call('getinfo').then(function(info){
+              console.log(d.name + ' info: ', info);
+            }, function(err){
+              console.log(d.name + ' :', err);
+            });
+          }, 15000);
+        })(coin.daemons[daemon]);
+      }
+    }
+    console.log(type.toUpperCase() + ': ' + port);
+  });
+};
+
+/* Create a daemon for each item in daemons */
+for(var type in {'sha256':true,'scrypt':true}) {
+
+  for (daemon in servers[type].daemons){
+
+    servers[type].daemons[daemon] =
+      new Stratum.Daemon(servers[type].daemons[daemon]);
+  }
+
+  /* SHA256 and Scrypt coins, our miner shouldnt worry which
+     type of coin is this
+  */
+  setup(type, servers[type]);
+}
+
